26bc8351079b50529faa2b1ce25a700e246cb13e
takahasi <takahasi@sample.com>
2016-11-01 18:05:30 +0900

    [modify] Modified test program to adapt sensing parameters

--- test/sensor_wacoh_udynpick.cpp
+++ test/sensor_wacoh_udynpick.cpp
@@ -25,6 +25,8 @@
 //#define USE_CUSTOM_FILTER (1)
 #define ERROR(str) printf("[ERR] %s:%d:%s\n", __FUNCTION__, __LINE__, str)
 
+static float sense[6] = {0, 0, 0, 0, 0, 0};
+
 /* PROTOTYPES */
 static void filter(_sensor_data *now);
 
@@ -61,62 +63,117 @@ int sensor_processor_wacoh_udynpick::init(void)
 
 int sensor_processor_wacoh_udynpick::setup(void)
 {
+    char str[100] = {0};
+
     /* waits for stabilizing */
     sleep(5);
 
-    /* set offset as initial values */
-    if (1 != write(fd, "O", 1)) {
-        ERROR("failed to write");
+    /* checks version information */
+    (void)read(fd, &str, sizeof(str)); /* dummy read */
+    memset(str, 0x00, sizeof(str));
+    (void)write(fd, "V", 1);
+
+    fd_set fds;
+    struct timeval tv;
+
+    tv.tv_sec = 2;
+    tv.tv_usec = 0;
+
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    if (select((fd + 1), &fds, NULL, NULL, &tv) < 0) {
+        /* select errors */
+        printf("select fd error!\n");
         return -1;
     }
-    if (1 != write(fd, "O", 1)) {
-        ERROR("failed to write");
-        return -1;
+    if (!FD_ISSET(fd, &fds)) {
+        /* select timeout or caught unknown signals */
     }
-    if (1 != write(fd, "O", 1)) {
-        ERROR("failed to write");
-        return -1;
+
+    (void)read(fd, &str, sizeof(str));
+    printf("\nVersion information:\n%s", str);
+
+    /* checks sensor specific parmeters */
+    while (1) {
+        memset(str, 0x00, sizeof(str));
+        (void)write(fd, "p", 1);
+
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
+        if (select((fd + 1), &fds, NULL, NULL, &tv) < 0) {
+            /* select errors */
+            printf("select fd error!\n");
+            return -1;
+        }
+        if (!FD_ISSET(fd, &fds)) {
+            /* select timeout or caught unknown signals */
+            continue;
+        }
+        (void)read(fd, &str, sizeof(str));
+        sscanf(str, "%f,%f,%f,%f,%f,%f",
+                &sense[0],
+                &sense[1],
+                &sense[2],
+                &sense[3],
+                &sense[4],
+                &sense[5]);
+        if ((sense[0] == 0) || (sense[1] == 0) || (sense[2] == 0)
+            || (sense[3] == 0) || (sense[4] == 0) || (sense[5] == 0)) {
+            continue;
+        }
+        printf("\nSensing paramters:\n");
+        printf("%4.3f, %4.3f, %4.3f, %4.3f, %4.3f, %4.3f\n",
+                sense[0],
+                sense[1],
+                sense[2],
+                sense[3],
+                sense[4],
+                sense[5]);
+        break;
     }
-    sleep(1);
 
     /* set low pass filter */
-    if (2 != write(fd, "1F", 2)) { /* 8F:8-points average */
+    if (2 != write(fd, "8F", 2)) { /* 8F:8-points average */
         ERROR("failed to write");
         return -1;
     }
-    sleep(1);
+
+    /* checks filter paramter */
+    (void)read(fd, &str, sizeof(str)); /* dummy read */
+    if (2 != write(fd, "0F", 2)) {
+        ERROR("failed to write");
+        return -1;
+    }
+
+    FD_ZERO(&fds);
+    FD_SET(fd, &fds);
+    if (select((fd + 1), &fds, NULL, NULL, &tv) < 0) {
+        /* select errors */
+        printf("select fd error!\n");
+        return -1;
+    }
+    if (!FD_ISSET(fd, &fds)) {
+        /* select timeout or caught unknown signals */
+    }
+
+    memset(str, 0x00, sizeof(str));
+    (void)read(fd, &str, sizeof(str));
+    printf("\nCurrent filter:\n%s", str);
+
+    /* set offset as initial values */
+    for (int i = 0; i < 3; i++) {
+        if (1 != write(fd, "O", 1)) {
+            ERROR("failed to write");
+            return -1;
+        }
+    }
 
     /* dummy read to set initial offset */
-    char str[100];
-    int len = 0;
-
     if (1 != write(fd, "R", 1)) {
         ERROR("failed to write");
         return -1;
     }
-    (void)read(fd, &str, 27);
-
-    sleep(1);
-
-    /* checks version information */
-    memset(str, 0x00, sizeof(str));
-    (void)write(fd, "V", 1);
-    len = read(fd, &str, 100);
-    printf("---\n");
-    for (int i = 0; i < len; i++) {
-        printf("%02x ", str[i]);
-    }
-    printf("---\n");
-
-    /* checks sensor specific parmeters */
-    memset(str, 0x00, sizeof(str));
-    (void)write(fd, "p", 1);
-    len = read(fd, &str, 100);
-    printf("---\n");
-    for (int i = 0; i < len; i++) {
-        printf("%02x ", str[i]);
-    }
-    printf("---\n");
+    (void)read(fd, &str, sizeof(str)); /* dummy read */
 
     return 0;
 }
@@ -135,7 +192,10 @@ int sensor_processor_wacoh_udynpick::get_data(_sensor_data *s)
 {
     char str[27];
 
-    /* read values */
+    /* dummy read */
+    (void)read(fd, &str, sizeof(str));
+
+    /* read values 1 time */
     if (1 != write(fd, "R", 1)) {
         ERROR("failed to write");
         return -1;
@@ -161,11 +221,13 @@ int sensor_processor_wacoh_udynpick::get_data(_sensor_data *s)
 
 int sensor_processor_wacoh_udynpick::show_data(_sensor_data *s)
 {
-    printf("[%d] ", s->tick);
-    for (int i = 0; i < AXIS_NUM; i++) {
-        printf("%d,", s->data[i] - 8192);
-    }
-    printf("\n");
+    printf("[%d]\n", s->tick);
+    printf(" - Fx : %f [N]\n", (s->data[0] - 8192) / sense[0]);
+    printf(" - Fy : %f [N]\n", (s->data[1] - 8192) / sense[1]);
+    printf(" - Fz : %f [N]\n", (s->data[2] - 8192) / sense[2]);
+    printf(" - Mx : %f [Nm]\n", (s->data[3] - 8192) / sense[3]);
+    printf(" - My : %f [Nm]\n", (s->data[4] - 8192) / sense[4]);
+    printf(" - Mz : %f [Nm]\n", (s->data[5] - 8192) / sense[5]);
 
     return 0;
 }
